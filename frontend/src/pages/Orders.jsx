6ce84649089f6fcec4d1fa6006c9b52da21842d0
unknown <joshuajulius2030@gmail.com>
2024-11-22 17:46:42 +0100

    payment options on place order page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+// import React, { useContext } from 'react'
+// import { ShopContext } from '../context/ShopContext'
+// import Title from '../components/Title';
 
-const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+// const Orders = () => {
+//   const {products, currency} = useContext(ShopContext); 
+//   return (
+//     <div className='border-t pt-16'>
+//       <div className='text-2xl'>
+//         <Title text1={'MY'} text2={'ORDERS'}/>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Orders
+// export default Orders
