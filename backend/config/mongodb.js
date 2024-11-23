ac98b358b0d9159972325f3d75e12433a0446c61
unknown <joshuajulius2030@gmail.com>
2024-11-23 20:52:33 +0100

    database connected

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    mongoose.connection.on('Connected', ()=>{
+    mongoose.connection.on('connected', ()=>{
         console.log('Db connected');
     })
     await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
