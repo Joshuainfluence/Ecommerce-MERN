0a1ca2b09b5e5b038a60c2605c97e142f28cdd4f
unknown <joshuajulius2030@gmail.com>
2024-11-24 02:01:55 +0100

    API endpoint working

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+// ROUTE FOR USER LOGIN
+const loginUser = async (req, res) => {
+    
+}
+
+// ROUTE FOR USER REGISTRATION
+
+const registerUser = async (req, res) => {
+    res.json({msg:"Register API working"})
+}
+
+// ROUTE FOR ADMIN LOGIN
+
+const adminLogin = async (req, res) => {
+    
+}
+
+
+
+export {loginUser, registerUser, adminLogin}
