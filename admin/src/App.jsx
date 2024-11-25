56d4e3aeefbe435ccd63df1851fd969d58ef9ecc
unknown <joshuajulius2030@gmail.com>
2024-11-25 15:43:11 +0100

    admin login logic, navbar  and sidebar components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Login from './components/Login'
+
+// thsis page is usjed for settinfg up he router rfo h  page
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
+
+
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
+
+  useEffect(()=>{
+    localStorage.setItem('token', token)
+  }, [token])
   return (
-    <div>
-      <h1>This is the admin panel</h1>
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer />
+
+      {token === "" ? <Login setToken={setToken} /> :
+        <>
+          <Navbar setToken={setToken} />
+
+          <hr />
+
+          <div className="flex w-full">
+
+            <Sidebar />
+
+            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add token={token}/>} />
+                <Route path='/list' element={<List token={token} />} />
+                <Route path='/orders' element={<Orders token={token} />} />
+
+
+
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
+
     </div>
   )
 }
--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,11 +4,13 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 
+
 export const assets = {
     logo,
     add_icon,
     order_icon,
     upload_area,
-    parcel_icon
+    parcel_icon,
+   
 }
 
